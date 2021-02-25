3ee5f2b3f551b91cb98c2853a2d3e32dbbff66d5
valerian <jean.huaringa@gmail.com>
2021-02-25 10:27:23 -0500

    comentarios controller

--- springboot-servicio-productos/src/main/java/com/formacionbdi/springboot/app/productos/controllers/ProductoController.java
+++ springboot-servicio-productos/src/main/java/com/formacionbdi/springboot/app/productos/controllers/ProductoController.java
@@ -16,7 +16,7 @@ public class ProductoController { //para levantear por instancias : -Dserver.por
 	@Autowired
 	private IProductoService productoService;
 	
-	//metodos hadler: metodos especificos
+	//metodos hadler
 	@GetMapping("/listar")
 	public List<Producto> listar(){
 		return productoService.finAll();

3e4d00ac922930c826a8c398278582efa0e1f8e6
valerian <jean.huaringa@gmail.com>
2021-02-27 20:57:22 -0500

    probando la configuracion conf-server

--- springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/controllers/ItemController.java
+++ springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/controllers/ItemController.java
@@ -1,9 +1,16 @@
 package com.formacionbdi.springboot.app.item.controllers;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,10 +24,15 @@ import com.netflix.ribbon.proxy.annotation.Hystrix;
 @RestController
 public class ItemController {
 	
+	private static Logger log = LoggerFactory.getLogger(ItemController.class);
+	
 	@Autowired
 	//@Qualifier("serviceFeign") //para espesificar cual implementacion usar.(feing o restTemplate) --> para usar el restTemplate @Qualifier("serviceRestTemplate")
 	@Qualifier("serviceRestTemplate")//para balanceo por serviceRestTemplate
 	private ItemService itemService;
+	
+	@Value("${configuracion.texto}")
+	private String texto;
 	
 	//Metodos Handler es un objeto que se encarga de una tarea especifica
 	
@@ -46,6 +58,15 @@ public class ItemController {
 		item.setProducto(producto);
 		return item;
 		
+	}
+	
+	@GetMapping("/obtener-config")
+	public ResponseEntity<?> obtenerConfig(@Value("${server.port}") String puerto){
+		log.info(texto);
+		Map<String,String> json = new HashMap<>();
+		json.put("texto",texto);
+		json.put("puerto", puerto);
+		return new ResponseEntity<Map<String,String>>(json,HttpStatus.OK);
 	}
 	
 }

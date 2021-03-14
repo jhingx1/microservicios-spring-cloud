7d1623bd5653e8cdeb3e0426ca48fa2308a9b31f
valerian <jean.huaringa@gmail.com>
2021-03-14 10:07:32 -0500

    exp id ms usuarios

---
+++ springboot-servicio-commons-usuarios/src/main/java/com/formacionbdi/springboot/app/commons/usuarios/SpringbootServicioCommonsUsuariosApplication.java
@@ -0,0 +1,16 @@
+package com.formacionbdi.springboot.app.commons.usuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+@EnableAutoConfiguration(exclude= {DataSourceAutoConfiguration.class})
+public class SpringbootServicioCommonsUsuariosApplication {
+
+//	public static void main(String[] args) {
+//		SpringApplication.run(SpringbootServicioCommonsUsuariosApplication.class, args);
+//	}
+
+}
--- springboot-servicio-usuarios/src/main/java/com/formacionbdi/springboot/app/usuarios/SpringbootServicioUsuariosApplication.java
+++ springboot-servicio-usuarios/src/main/java/com/formacionbdi/springboot/app/usuarios/SpringbootServicioUsuariosApplication.java
@@ -2,8 +2,12 @@ package com.formacionbdi.springboot.app.usuarios;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+//@EntityScan({"com.formacionbdi.springboot.app.commons.usuarios.models.entity"})
 
 @SpringBootApplication
+@EntityScan({"com.formacionbdi.springboot.app.commons.usuarios.models.entity"})
 public class SpringbootServicioUsuariosApplication {
 
 	public static void main(String[] args) {
